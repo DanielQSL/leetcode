d28e98a01f05eb0faa5c200274408d498d743f3e
qianshuailong <qianshuailong@mallcoo.cn>
2018-07-09 15:56:29 +0800

    add NO.20 test case

--- 20.Valid-Parentheses/src/Solution.java
+++ 20.Valid-Parentheses/src/Solution.java
@@ -21,4 +21,9 @@ public class Solution {
         }
         return stack.isEmpty();
     }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).isValid("()[]{}"));
+        System.out.println((new Solution()).isValid("([}"));
+    }
 }
