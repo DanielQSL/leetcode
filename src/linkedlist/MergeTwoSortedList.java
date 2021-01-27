1bd228d871bc3fc252c871cd6ef5231f26a9b640
qianshuailong <qianshuailong@hupu.com>
2021-01-27 18:12:26 +0800

    feat:新增链表题

---
+++ src/linkedlist/MergeTwoSortedList.java
@@ -0,0 +1,46 @@
+package linkedlist;
+
+/**
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * @author qianshuailong
+ * @date 2021/1/27
+ */
+public class MergeTwoSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 递归法
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+}
