00a2272ca49b768e37b1b2359d9f8f975234160b
danielqsl <qiansl0316@126.com>
2021-02-14 17:34:23 +0800

    refactor:修复找出第一个错误版本

--- src/binarysearch/FirstBadVersion.java
+++ src/binarysearch/FirstBadVersion.java
@@ -1,11 +1,15 @@
 package binarysearch;
 
 /**
- * 278. First Bad Version
+ * 278. 第一个错误的版本
  * <p>
- * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
- * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
- * You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-bad-version
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * <p>
  * Example:
  * Given n = 5, and version = 4 is the first bad version.
@@ -25,20 +29,17 @@ public class FirstBadVersion {
      * 思路：
      * 使用二分查找进行判断
      * 需要注意的是：如果发现当前版本是好的，则需要将left指向mid的下一个位置。
-     *
+     * <p>
      * Time: O(logn)
      * Space: O(1)
-     * @param n
-     * @return
      */
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
 
         while (left < right) {
-            //找到中间索引
-            int mid = left + (right - left) >> 1;
-
+            // 找到中间索引。由于(left+right)/2会有整数溢出的情况，故用一下方法代替
+            int mid = left + ((right - left) >> 1);
             if (isBadVersion(mid)) {
                 right = mid;
             } else {
