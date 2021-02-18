2002636685037aa8d78ea8fa7e8f1d156d873782
qianshuailong <qianshuailong@hupu.com>
2021-02-18 11:01:41 +0800

    feat:NO.876链表中间结点

---
+++ src/linkedlist/MiddleNode.java
@@ -0,0 +1,46 @@
+package linkedlist;
+
+/**
+ * 876. 链表的中间结点
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ * @author qianshuailong
+ * @date 2021/2/18
+ */
+public class MiddleNode {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 快慢指针
+     */
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
